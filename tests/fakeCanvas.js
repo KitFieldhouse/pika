70a42c666781b240382e67e070e9bcc9d712735f
KitFieldhouse <kit.fieldhouse@gmail.com>
2024-10-18 22:38:47 -0500

    fake canvas will use a fakeGL context

--- tests/fakeCanvas.js
+++ tests/fakeCanvas.js
@@ -1,7 +1,4 @@
-
-
-let fakeGL = {FLOAT: 1, HALF_FLOAT: 2, SHORT: 3, UNSIGNED_SHORT: 4, BYTE: 5, UNSIGNED_BYTE: 6, 
-             INT: 7, UNSIGNED_INT: 8};
+import fakeGL from "./fakeGL";
 
 let fakeCanvas = {};
 
