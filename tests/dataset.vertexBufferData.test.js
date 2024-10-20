bd52e8c3320b90d3402c3efe4e267fe101bc3079
KitFieldhouse <kit.fieldhouse@gmail.com>
2024-10-20 00:59:24 -0500

    Add test that data is appended correctly

--- tests/dataset.vertexBufferData.test.js
+++ tests/dataset.vertexBufferData.test.js
@@ -9,7 +9,7 @@ import fakeCanvas from './fakeCanvas.js';
 
 
 
-test("Test that a dataStore can be initialized", () => {
+test("Test that a vertex buffer dataStore can be initialized", () => {
   const gl = new GL(fakeCanvas);
 
   let inputs = {
@@ -26,6 +26,43 @@ test("Test that a dataStore can be initialized", () => {
   expect(gl.gl.tests_buffers[0].byteLength).toBe(800);
 
 });
+
+
+
+test("Test datastore append on a vertex buffer dataStore", () => {
+  const gl = new GL(fakeCanvas);
+
+  let inputs = {
+    y: {name: 'y', size: 1, type: 'float'},
+    x: {name: 'x', size: 1, type: 'float'}
+  };
+
+  let dataset = gl.createDataSet({
+    inputs: Object.values(inputs),
+    layout: [GL.VertexBuffer( [GL.repeat('x', 'y')] )]
+  });
+
+  let data = [1,2,3,4]
+
+  dataset.appendData(data, [GL.repeat('y', 'x')]);
+
+  expect(gl.gl.tests_buffers.length).toBe(1);
+  expect(gl.gl.tests_buffers[0].byteLength).toBe(800);
+
+
+  let reconstructedData = [];
+  let view = new DataView(gl.gl.tests_buffers[0])
+
+  for(let i = 0; i < 16; i = i + 4){
+    reconstructedData.push(view.getFloat32(i, true))
+  }
+
+  console.log(reconstructedData);
+
+  expect(reconstructedData).toEqual([2,1,4,3]);
+
+});
+
 
 
 
