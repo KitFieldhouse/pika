f974cd952bede0f63c910dc977e98dddf3a0166b
KitFieldhouse <kit.fieldhouse@gmail.com>
2024-10-21 02:47:11 -0500

    Add test for more complex data layouts with mixed data types

--- tests/dataset.vertexBufferData.test.js
+++ tests/dataset.vertexBufferData.test.js
@@ -131,7 +131,6 @@ test("Test datastore append on a vertex buffer dataStore, same layout, direct co
 
   let data = new Float32Array([1,2,3,4]);
 
-  console.log(data.buffer instanceof ArrayBuffer);
 
   let effects = dataset.appendData(data.buffer, [GL.repeat('x', 'y')]);
 
@@ -148,9 +147,98 @@ test("Test datastore append on a vertex buffer dataStore, same layout, direct co
     reconstructedData.push(view.getFloat32(i, true))
   }
 
-  console.log(reconstructedData);
-
   expect(reconstructedData).toEqual([1,2,3,4]);
+
+});
+
+
+test("Test datastore append on several vertex buffer dataStores, complex layout, no direct copy, mixed data types", () => {
+  const gl = new GL(fakeCanvas);
+
+  let inputs = {
+    y: {name: 'y', size: 1, type: 'float'},
+    x: {name: 'x', size: 1, type: 'float'},
+    w: {name: 'w', size: 1, type: 'float'},
+    z: {name: 'z', size: 1, type: 'float'}
+  };
+
+  let dataset = gl.createDataSet({
+    inputs: Object.values(inputs),
+    layout: [GL.VertexBuffer( [GL.repeat('x', 'y')]), GL.VertexBuffer( [GL.repeat('w')]), GL.VertexBuffer( [GL.repeat('z')])]
+  });
+
+  let dataXY = new Float32Array([1,2,3,4]);
+
+  let data = [[dataXY.buffer], [5,6,7,8], [[-1,-2,-3,-4], -5, -6], -7, -8]
+  let dataDescriptor = [[GL.repeat('x','y')], [GL.repeat('y', 'x')], [[GL.repeat('w', 'z')], GL.repeat("w")], GL.repeat('z')];
+
+  let effects = dataset.appendData(data, dataDescriptor); 
+
+  expect(effects.numberOfDirectCopies).toEqual([0,0,0])
+
+  expect(gl.gl.tests_buffers.length).toBe(3);
+  expect(gl.gl.tests_buffers[0].byteLength).toBe(800);
+  expect(gl.gl.tests_buffers[1].byteLength).toBe(400);
+  expect(gl.gl.tests_buffers[2].byteLength).toBe(400);
+
+  expect(Array.from(new Float32Array(gl.gl.tests_buffers[0].slice(0, 8*4)))).toEqual([1,2,3,4,6,5,8,7]);
+  expect(Array.from(new Float32Array(gl.gl.tests_buffers[1].slice(0, 4*4)))).toEqual([-1, -3, -5, -6]);
+  expect(Array.from(new Float32Array(gl.gl.tests_buffers[2].slice(0, 4*4)))).toEqual([-2, -4, -7, -8]);
+
+  // expect();
+
+});
+
+
+
+test("Test datastore append on a vertex buffer dataStore, same layout, direct copy, complex data layout", () => {
+  const gl = new GL(fakeCanvas);
+
+  let inputs = {
+    y: {name: 'y', size: 1, type: 'float'},
+    x: {name: 'x', size: 1, type: 'float'},
+    w: {name: 'w', size: 1, type: 'float'},
+    z: {name: 'z', size: 1, type: 'float'}
+  };
+
+  let dataset = gl.createDataSet({
+    inputs: Object.values(inputs),
+    layout: [GL.VertexBuffer( [GL.repeat('x', 'y')] ), GL.VertexBuffer( [GL.repeat('w', 'z')])]
+  });
+
+  let xyData = new Float32Array([1,2,3,4]);
+
+  let wzData = new Float32Array([5,6,7,8]);
+
+  let data = [ [[[xyData.buffer]]] , [[[[wzData.buffer]]]] ]
+
+  let effects = dataset.appendData(data, [[ [ [GL.repeat('x', 'y')] ]], [[[[GL.repeat('w', 'z')]]]]]);
+
+  expect(effects.numberOfDirectCopies).toEqual([1,1])
+
+  expect(gl.gl.tests_buffers.length).toBe(2);
+  expect(gl.gl.tests_buffers[0].byteLength).toBe(800);
+  expect(gl.gl.tests_buffers[1].byteLength).toBe(800);
+
+
+  let reconstructedDataXY = [];
+  let view = new DataView(gl.gl.tests_buffers[0])
+
+  for(let i = 0; i < 16; i = i + 4){
+    reconstructedDataXY.push(view.getFloat32(i, true))
+  }
+
+  let reconstructedDataWZ = [];
+  view = new DataView(gl.gl.tests_buffers[1])
+
+  for(let i = 0; i < 16; i = i + 4){
+    reconstructedDataWZ.push(view.getFloat32(i, true))
+  }
+
+
+  expect(reconstructedDataXY).toEqual([1,2,3,4]);
+
+  expect(reconstructedDataWZ).toEqual([5,6,7,8]);
 
 });
 
